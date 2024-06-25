79593ecf26d51d9684d3498adf9b7ee2ed89898b
luis-domingues <luisdomingues117@gmail.com>
2024-06-25 20:19:24 -0300

    Implementation of withdrawal functionality and code improvement

--- Models/ClientAccount.cs
+++ Models/ClientAccount.cs
@@ -8,17 +8,43 @@ namespace AppBancario.Models
     public class ClientAccount
     {
         public string ClientName { get; set; }
-        private decimal Balance { get; set; }
+        public decimal Balance { get; set; }
+
+        public ClientAccount(string clientName, decimal initialBalance = 0M)
+        {
+            ClientName = clientName;
+            Balance = initialBalance;
+        }
 
         public void AccountAccess()
         {
-            Console.WriteLine($"\nOlá, {ClientName}! Tudo bem?");
+            Console.WriteLine($"Olá, {ClientName}! Tudo bem?");
         }
 
         public void ShowBalance()
         {
-            decimal Balance = 1000M;
             Console.WriteLine($"\nSALDO DISPONÍVEL: {Balance:c}");
+        }
+
+        public bool WithdrawMoney(decimal amount)
+        {
+            try
+            {
+                if (amount > Balance)
+                {
+                    Console.WriteLine("SALDO INSUFICIENTE");
+                    return false;
+                }
+
+                Balance -= amount;
+                Console.WriteLine($"\nSAQUE DE {amount:c} REALIZADO!");
+                return true;
+            }
+            catch (Exception erroAmount)
+            {
+                Console.WriteLine($"ERRO DE SAQUE {erroAmount.Message}");
+                return true;
+            }
         }
     }
 }
