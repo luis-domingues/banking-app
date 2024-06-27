1e88833e806c939dd859917fe85a62fa66ec83b3
luis-domingues <luisdomingues117@gmail.com>
2024-06-27 20:46:07 -0300

    implementation of the Deposit() method and add NLog.config file to configure error logging

--- Models/ClientAccount.cs
+++ Models/ClientAccount.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog;
 
 namespace AppBancario.Models
 {
@@ -44,6 +45,47 @@ namespace AppBancario.Models
             {
                 Console.WriteLine($"ERRO DE SAQUE {erroAmount.Message}");
                 return true;
+            }
+        }
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public bool Deposit(decimal depositAmount)
+        {
+            try
+            {
+                if (depositAmount <= 0)
+                {
+                    Console.Clear();
+                    throw new ArgumentException("Valor do depósito deve ser positivo.");
+                }
+
+                Balance += depositAmount;
+                Console.WriteLine($"Depósito de {depositAmount:c} realizado com sucesso!");
+                return true;
+            }
+            catch (FormatException erroDeposit)
+            {
+                logger.Error($"ERRO DE DEPÓSITO. DIGITE UM NÚMERO VÁLIDO. {erroDeposit}");
+                Console.Write("ERRO DE DEPÓSITO. DIGITE UM NÚMERO VÁLIDO.");
+                return false;
+            }
+            catch (OverflowException erroSurplusValue)
+            {
+                logger.Error($"ERRO DE VALOR. O DEPÓSITO EXCEDE O LIMETE DA CONTA. {erroSurplusValue}");
+                Console.Write($"ERRO DE VALOR. O DEPÓSITO EXCEDE O LIMETE DA CONTA.");
+                return false;
+            }
+            catch (ArgumentException erroDecimalPositive)
+            {
+                logger.Error($"ERRO DE DEPÓSITO. DIGITE UM VALOR POSITIVO. {erroDecimalPositive}");
+                Console.Write($"ERRO DE DEPÓSITO. DIGITE UM VALOR POSITIVO.");
+                return false;
+            }
+            catch (Exception erroGenericExecution)
+            {
+                logger.Error($"Erro inesperado durante o depósito. {erroGenericExecution}");
+                Console.WriteLine($"Erro inesperado durante o depósito.");
+                return false;
             }
         }
     }
