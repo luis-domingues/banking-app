76d5a35c6dcf36fa9a59fdc00cf4b356261485da
luis-domingues <luisdomingues117@gmail.com>
2024-09-02 15:03:01 -0300

    feat: create Card class to represent customer card information

--- Models/Card.cs
+++ Models/Card.cs
@@ -1,6 +1,9 @@
-﻿namespace DefaultNamespace;
+﻿namespace BankingApp.Models;
 
 public class Card
 {
-    
+    public string Number { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public string Brand { get; set; }
+    public decimal Limit { get; set; }
 }
