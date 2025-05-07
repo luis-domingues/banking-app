91f918764c5c84f109ab7d0357cc25ae203a6e59
luis-domingues <luisdomingues117@gmail.com>
2025-05-07 16:42:00 -0300

    feat: removing olds file

--- Models/Card.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BankingApp.Models;
-
-public class Card
-{
-    public string Number { get; set; }
-    public DateTime ExpirationDate { get; set; }
-    public string Brand { get; set; }
-    public decimal Limit { get; set; }
-}
